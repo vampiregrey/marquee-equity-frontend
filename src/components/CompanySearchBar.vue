e9d99011f8853a947be3ec4ed768e994808444e4
Shubham Maurya <shubham.maurya.co@outlook.com>
2020-12-09 12:44:52 +0530

    added function to post data to backend, show message on successful data saved.

--- src/components/CompanySearchBar.vue
+++ src/components/CompanySearchBar.vue
@@ -10,13 +10,13 @@
         </div>
         <div class="row py-3">
             <div class="col-md-6 offset-md-3 text-center">
-                <button class="btn btn-primary">Submit</button>
+                <button class="btn btn-primary" @click.prevent="saveCompanyInfo">Submit</button>
             </div>
         </div>
 
         <div class="row py-3">
             <div class="col-md-6 offset-md-3 text-center">
-               <div v-html="companyDataBox"></div>
+               <div id="listCompany" v-html="companyDataBox"></div>
             </div>
         </div>
     </div>
@@ -32,7 +32,9 @@
           return{
               companyName : '',
               processing : false,
-              companyDataBox: ''
+              postingData: false,
+              companyDataBox: '',
+              companyArray: [],
           }
         },
         methods:{
@@ -44,7 +46,6 @@
                 }
 
                 //check if we are not sending empty string and it is greater than 1
-                console.log(this.companyName.length);
                 if (this.companyName.length <= 1){
 
                     // clear company name
@@ -69,11 +70,62 @@
                         "x-requested-with": "XMLHttpRequest"
                     }
                 }).then(result => {
-                    console.log(result.data);
                     this.companyDataBox = result.data;
                     this.processing = false;
                 }).catch(error => {
                     this.processing = false;
+                    console.log(error)
+                });
+            },
+            saveCompanyInfo(){
+                let companiesListing = document.querySelectorAll("#listCompany div");
+
+                //check to make sure we don't overflow the api
+                if (this.postingData===true){
+                    return;
+                }
+
+                //Empty array data to prevent overriding
+                this.companyArray = [];
+                for (let i = 0; i<companiesListing.length; i++) {
+
+                    let extractNameAndCin = companiesListing[i].id.split('/');
+
+                    //console.log(extractNameAndCin);
+                    this.companyArray[i] = {
+                        "name": extractNameAndCin[1],
+                        "cin": extractNameAndCin[2]
+                    }
+                }
+
+                //Post data to server to process saving to database
+
+                axios({
+                    method: 'post',
+                    url: "http://localhost:3000/save",
+                    data: {
+                        companies : this.companyArray,
+                    },
+                    header:{
+                        "accept": "*/*",
+                        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+                        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        "sec-fetch-dest": "empty",
+                        "sec-fetch-mode": "cors",
+                        "sec-fetch-site": "same-origin",
+                        "x-requested-with": "XMLHttpRequest"
+                    }
+                }).then(() => {
+                    this.$toasted.success('Data is saved to database' , {position: "top-center",
+                        duration : 2500})
+                    this.postingData = false;
+
+                    //reset the search
+
+                    this.companyDataBox = '';
+                    this.companyName ='';
+                }).catch(error => {
+                    this.postingData = false;
                     console.log(error)
                 });
             }
